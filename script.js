12e7595d1ba3ead4e82b7d6991530263fa43a736
Oneluv1 <adegunju27@gmail.com>
2023-05-06 08:24:08 +0100

    1

---
+++ script.js
@@ -0,0 +1,12 @@
+/* myFunction(){
+  const buttonElement = document.querySelector('.click');
+  //const buttonElement = document.body('.click');
+  //if (button.innerText === 'Toggle Dark Mode!') {
+  buttonElement.addEventListener('click', () => ('dark-mode'))
+}
+}*/
+function hello() {
+  const element = document.body;
+  element.classList.toggle("dark-mode");
+  
+}
